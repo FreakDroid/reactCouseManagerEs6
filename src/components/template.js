9e50062d5ee6d3d6ac024d08d642d1629389dc25
Wilfredo Perez <itwarp_wperez@iseatz.com>
2017-04-13 15:38:12 -0400

    First commit about the project

---
+++ src/components/template.js
@@ -0,0 +1,19 @@
+import React, {PropTypes} from 'react';
+import Header from './common/Header';
+
+class Template extends React.Component{
+  render(){
+    return (
+      <div className="container-fluid">
+        <Header />
+        {this.props.children}
+      </div>
+    );
+  }
+}
+
+Template.PropTypes = {
+  children : PropTypes.object.isRequired
+};
+
+export default Template;
